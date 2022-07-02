e147b3461d211f622116759c503748bf82f92c51
Hyeondeok <rlagusejr135@naver.com>
2022-07-02 11:46:17 +0900

    feat : parsing add : new_word.txt add  : hangman.py

--- parsing.py
+++ parsing.py
@@ -1,16 +1,15 @@
-# 한줄씩 읽어서 앞에 2칸 지우기
-# but 알파벳이 아니면 지우기
-#
-f = open('C:/Users/ASUS/Desktop/words.txt', 'r', encoding="ANSI")
-word = f.read()
+word = open('C:/Users/ASUS/Desktop/word.txt', 'r', encoding="ANSI")
+new_word = open('new_word.txt', 'w', encoding="ANSI")
 while True:
-    bowl_line = []
-    del_word = '□ '
-    line = f.readline()
+    flag = False
+    line = word.readline()
     for i in range(len(line)):
-        if 'a' <= line[i] <= "z" or 'A' <= line[i] <='Z':
-            bowl_line.append(line[i])
-    line = bowl_line
-
-print(word)
-f.close()
+        if 'a' <= line[i] <= "z" or 'A' <= line[i] <= 'Z':
+            print(line[i], file=new_word, end='')
+            flag = True
+    if flag:
+        print(file=new_word)
+    if line == '':
+        break
+word.close()
+new_word.close()
